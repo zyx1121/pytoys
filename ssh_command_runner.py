11acb23469441df1c5f4a56cac7dfdd40fd55302
zyx1121 <yongxiang.zhan@outlook.com>
2024-06-05 14:03:23 +0800

    Improve visual design

--- ssh_command_runner.py
+++ ssh_command_runner.py
@@ -6,50 +6,71 @@ to initialize a default configuration, print logs of previous command executions
 specify a configuration file for the current run.
 
 Usage:
-    --init             Initialize the configuration directory with default values.
-    --log <config>     Print the logs for the specified configuration file.
-    --cfg <config>     Specify the configuration file (without .yaml extension).
+    init             Initialize the configuration directory with default values.
+    log <config>     Print the logs for the specified configuration file.
+    run <config>     Specify the configuration file (without .yaml extension).
 
 Example:
-    python ssh_command_runner.py --cfg my_config
-    python ssh_command_runner.py --log my_config
+    python ssh_command_runner.py run my_config
+    python ssh_command_runner.py log my_config
 
 Directory structure:
     Configurations: ~/.pytoys/configs/
     Logs: ~/.pytoys/logs/
 """
 
-import argparse
-import os
-import sys
-import termios
+import logging
 import time
-import tty
 from concurrent.futures import ThreadPoolExecutor, as_completed
-from datetime import datetime
+from pathlib import Path
 
 import paramiko
+import typer
 import yaml
-from tqdm import tqdm
+from rich import print
+from rich.console import Console
+from rich.panel import Panel
+from rich.progress import (
+    BarColumn,
+    Progress,
+    TextColumn,
+    TimeElapsedColumn,
+    TimeRemainingColumn,
+)
+from rich.prompt import Confirm
 
-CONFIG_DIR = os.path.expanduser("~/.pytoys/configs/")
-LOG_DIR = os.path.expanduser("~/.pytoys/logs/")
+app = typer.Typer()
+console = Console()
+
+CONFIG_DIR = Path.home() / ".pytoys" / "configs"
+LOG_DIR = Path.home() / ".pytoys" / "logs"
 DEFAULT_TIMEOUT = 120
 
 
 def ensure_directories_exist():
-    if not os.path.exists(CONFIG_DIR):
-        os.makedirs(CONFIG_DIR)
-    if not os.path.exists(LOG_DIR):
-        os.makedirs(LOG_DIR)
+    CONFIG_DIR.mkdir(parents=True, exist_ok=True)
+    LOG_DIR.mkdir(parents=True, exist_ok=True)
 
 
-def log_with_timestamp(log_file, message):
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    log_file.write(f"[{timestamp}] {message}\n")
+def setup_logger(config_name, device_name, clear_logs=False):
+    log_file_path = LOG_DIR / f"{config_name}_{device_name}.log"
+    logger = logging.getLogger(f"{config_name}_{device_name}")
+
+    if not logger.handlers:  # Check if handlers are already set
+        logger.setLevel(logging.DEBUG)
+        fh = logging.FileHandler(log_file_path, mode="w" if clear_logs else "a")
+        fh.setLevel(logging.DEBUG)
+        formatter = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
+        )
+        fh.setFormatter(formatter)
+        logger.addHandler(fh)
+
+    return logger
 
 
-def execute_ssh_command(ssh, command, timeout=DEFAULT_TIMEOUT):
+def execute_ssh_command(ssh, command, logger, timeout=DEFAULT_TIMEOUT):
     stdin, stdout, stderr = ssh.exec_command(command)
 
     output = ""
@@ -71,76 +92,116 @@ def execute_ssh_command(ssh, command, timeout=DEFAULT_TIMEOUT):
     return output, error
 
 
-def connect_ssh(hostname, username, password, port=22):
+def connect_ssh(hostname, username, password, logger, port=22, retries=3):
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-    try:
-        ssh.connect(hostname, port, username, password, timeout=10)
-        return ssh
-    except paramiko.AuthenticationException:
-        print(f"Authentication failed when connecting to {hostname}")
-    except paramiko.SSHException as sshException:
-        print(f"Could not establish SSH connection: {sshException}")
-    except Exception as e:
-        print(f"Exception in connecting to {hostname}: {e}")
+    for attempt in range(retries):
+        try:
+            ssh.connect(hostname, port, username, password, timeout=10)
+            logger.info(f"Connected to {hostname}\n")
+            return ssh
+        except paramiko.AuthenticationException:
+            logger.error(f"Authentication failed when connecting to {hostname}")
+        except paramiko.SSHException as sshException:
+            logger.error(f"Could not establish SSH connection: {sshException}")
+        except Exception as e:
+            logger.error(f"Exception in connecting to {hostname}: {e}")
+
+        time.sleep(5)  # Wait before retrying
+
     return None
 
 
-def run_commands_on_device(device, commands, log_dir, config_name, progress_bar):
+def run_commands(device, commands, config_name, task_id, progress):
     hostname = device["hostname"]
     port = device.get("port", 22)
     username = device["username"]
     password = device["password"]
-    log_file_path = os.path.join(log_dir, f"{config_name}_{hostname}.log")
+    logger = setup_logger(config_name, hostname, clear_logs=True)
 
-    with open(log_file_path, "w") as log_file:
-        ssh = connect_ssh(hostname, username, password, port)
-        if not ssh:
-            log_with_timestamp(log_file, f"Failed to connect to {hostname}")
-            progress_bar.update(len(commands))  # Update the progress bar
-            return
+    ssh = connect_ssh(hostname, username, password, logger, port)
+    if not ssh:
+        logger.error(f"Failed to connect to {hostname}")
+        progress.update(task_id, advance=len(commands))
+        return
 
-        for cmd in commands:
-            command = cmd.get("command")
-            timeout = cmd.get("timeout", DEFAULT_TIMEOUT)
-            sleep_time = cmd.get("sleep", 0)
+    for cmd in commands:
+        command = cmd.get("command")
+        timeout = cmd.get("timeout", DEFAULT_TIMEOUT)
+        sleep_time = cmd.get("sleep", 0)
 
-            if command:
-                try:
-                    log_with_timestamp(log_file, f"Executing command:\n{command}")
-                    output, error = execute_ssh_command(ssh, command, timeout)
-                    if error:
-                        log_with_timestamp(log_file, f"Error on {hostname}:\n{error}")
-                    else:
-                        log_with_timestamp(log_file, f"Output on {hostname}:\n{output}")
-                except TimeoutError as e:
-                    log_with_timestamp(log_file, f"Timeout on {hostname}:\n{e}")
-                    ssh.close()
-                    log_with_timestamp(log_file, f"Reconnecting to {hostname}...")
-                    ssh = connect_ssh(hostname, username, password, port)
-                    if not ssh:
-                        log_with_timestamp(
-                            log_file, f"Failed to reconnect to {hostname}"
-                        )
-                        break
-            if sleep_time > 0:
-                log_with_timestamp(log_file, f"Sleeping for {sleep_time} seconds")
-                time.sleep(sleep_time)
+        if command:
+            try:
+                logger.info(f"Executing command:\n{command}\n")
+                output, error = execute_ssh_command(ssh, command, logger, timeout)
+                if error:
+                    logger.error(f"Error:\n{error}")
+                else:
+                    logger.info(f"Output:\n{output}")
+            except TimeoutError as e:
+                logger.error(f"Timeout on {hostname}: {e}")
+                ssh.close()
+                logger.info(f"Reconnecting to {hostname}...")
+                ssh = connect_ssh(hostname, username, password, logger, port)
+                if not ssh:
+                    logger.error(f"Failed to reconnect to {hostname}")
+                    break
+        if sleep_time > 0:
+            logger.info(f"Sleeping for {sleep_time} seconds")
+            time.sleep(sleep_time)
 
-            progress_bar.update(1)
+        progress.update(task_id, advance=1)
 
-        if ssh:
-            ssh.close()
+    if ssh:
+        ssh.close()
 
 
-def load_config(config_file):
-    with open(config_file, "r") as file:
+def load_config(config_file: Path):
+    with config_file.open("r") as file:
         config = yaml.safe_load(file)
     return config
 
 
-def init_config(config_dir):
+def print_config(config):
+    devices_text = "\n".join(
+        [
+            f"[cyan]Hostname:[/cyan] {device['hostname']}\n[cyan]Port:[/cyan] {device.get('port', 22)}\n[cyan]Username:[/cyan] {device['username']}\n[cyan]Password:[/cyan] {device['password']}\n"
+            for device in config["devices"]
+        ]
+    )
+    commands_text = "\n".join(
+        [
+            f"[cyan]Command:[/cyan] {cmd.get('command', 'sleep')}\n[cyan]Timeout:[/cyan] {cmd.get('timeout', DEFAULT_TIMEOUT)} seconds\n[cyan]Sleep:[/cyan] {cmd.get('sleep', 0)} seconds\n"
+            for cmd in config["commands"]
+            if "command" in cmd or "sleep" in cmd
+        ]
+    )
+    console.print(
+        Panel(
+            devices_text,
+            title="Configuration",
+            title_align="left",
+            border_style="dim",
+        )
+    )
+    console.print(
+        Panel(
+            commands_text,
+            title="Commands",
+            title_align="left",
+            border_style="dim",
+        )
+    )
+
+
+@app.command()
+def init():
+    """
+    Initialize the configuration directory with default values.
+    """
+    ensure_directories_exist()
+
     default_config = {
         "devices": [
             {
@@ -157,161 +218,129 @@ def init_config(config_dir):
         ],
     }
 
-    if not os.path.exists(config_dir):
-        os.makedirs(config_dir)
-
-    config_file = os.path.join(config_dir, "default_config.yaml")
-    with open(config_file, "w") as file:
+    config_name = typer.prompt("Enter the configuration name", default="default_config")
+    config_file = CONFIG_DIR / f"{config_name}.yaml"
+    with config_file.open("w") as file:
         yaml.dump(default_config, file)
-    print(f"Default configuration initialized at {config_file}")
-
-
-def print_config(config):
-    print("\nDevices:")
-    for device in config["devices"]:
-        print(f"  - Hostname: {device['hostname']}")
-        print(f"    Port: {device.get('port', 22)}")
-        print(f"    Username: {device['username']}")
-        print(f"    Password: {device['password']}\n")
-
-    print("Commands:")
-    for cmd in config["commands"]:
-        command = cmd.get("command", "sleep")
-        timeout = cmd.get("timeout", DEFAULT_TIMEOUT)
-        sleep_time = cmd.get("sleep", 0)
-        if command != "sleep":
-            print(f"  - Command: {command}")
-            print(f"    Timeout: {timeout} seconds\n")
-        if sleep_time > 0:
-            print(f"  - Sleep: {sleep_time} seconds\n")
-
-
-def print_log(config_name):
-    config_file = os.path.join(CONFIG_DIR, config_name + ".yaml")
-    if not os.path.isfile(config_file):
-        print(f"Configuration file '{config_file}' does not exist.")
-        return
-    config = load_config(config_file)
-
-    hostnames = [device["hostname"] for device in config["devices"]]
-
-    log_files = [f for f in os.listdir(LOG_DIR) if f.startswith(config_name)]
-
-    if not log_files:
-        print(f"No log files found for configuration '{config_name}'.")
-        return
-
-    matched_log_files = [f for f in log_files if any(n in f for n in hostnames)]
-
-    for log_file in matched_log_files:
-        log_file_path = os.path.join(LOG_DIR, log_file)
-        print(f"\nLog file: {log_file_path}\n")
-        with open(log_file_path, "r") as file:
-            print(file.read())
-
-
-def wait_for_user_input():
-    print(
-        "Please review the above configuration and press Enter to continue, or press ESC/Ctrl+C to cancel.\n"
+    console.print(
+        Panel(
+            f"Default configuration initialized at {config_file}",
+            style="green",
+            border_style="dim",
+        )
     )
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    try:
-        tty.setcbreak(fd)
-        while True:
-            ch = sys.stdin.read(1)
-            if ch == "\n":
-                break
-            elif ch in ["\x1b", "\x03"]:  # ESC or Ctrl+C
-                raise KeyboardInterrupt
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
 
 
-def main():
-    parser = argparse.ArgumentParser(
-        description="Run commands on multiple DUTs via SSH."
-    )
-    parser.add_argument(
-        "--init",
-        action="store_true",
-        help="Initialize the configuration directory with default values",
-    )
-    parser.add_argument(
-        "--log",
-        type=str,
-        help="Print the logs for the specified configuration file",
-    )
-    parser.add_argument(
-        "--cfg",
-        type=str,
-        help="Specify the configuration file (without .yaml extension)",
-    )
-    args = parser.parse_args()
-
+@app.command()
+def log(config_name: str):
+    """
+    Print the logs for the specified configuration file.
+    """
     ensure_directories_exist()
 
-    if args.init:
-        init_config(CONFIG_DIR)
+    config_file = CONFIG_DIR / f"{config_name}.yaml"
+    if not config_file.is_file():
+        console.print(
+            Panel(
+                f"Configuration file '{config_file}' does not exist.", style="bold red"
+            )
+        )
+        return
+    config_data = load_config(config_file)
 
-    elif args.log:
-        print_log(args.log)
+    hostnames = [device["hostname"] for device in config_data["devices"]]
 
-    elif args.cfg:
-        config_file = os.path.join(CONFIG_DIR, args.cfg + ".yaml")
-        if not os.path.isfile(config_file):
-            print(f"Configuration file '{config_file}' does not exist.")
-            return
-        config = load_config(config_file)
-        print_config(config)
+    log_files = [f for f in LOG_DIR.iterdir() if f.name.startswith(config_name)]
 
-        try:
-            wait_for_user_input()
-        except KeyboardInterrupt:
-            print("Exiting...")
-            return
+    if not log_files:
+        console.print(
+            Panel(
+                f"No log files found for configuration '{config_name}'.",
+                style="bold red",
+            )
+        )
+        return
 
-        commands = config["commands"]
-        devices = config["devices"]
-        log_dir = LOG_DIR
+    matched_log_files = [f for f in log_files if any(n in f.name for n in hostnames)]
 
-        config_name = args.cfg
+    for log_file in matched_log_files:
+        console.print(
+            Panel(
+                f"{log_file}", title="Log file", title_align="left", border_style="dim"
+            )
+        )
+        with log_file.open("r") as file:
+            console.print(file.read())
 
-        total_tasks = len(devices) * len(commands)
-        with tqdm(
-            total=total_tasks, desc="Executing commands", unit="task"
-        ) as progress_bar:
-            with ThreadPoolExecutor(max_workers=len(devices)) as executor:
-                futures = {
-                    executor.submit(
-                        run_commands_on_device,
-                        device,
-                        commands,
-                        log_dir,
-                        config_name,
-                        progress_bar,
-                    ): device
-                    for device in devices
-                }
-                for future in as_completed(futures):
-                    device = futures[future]
-                    try:
-                        future.result()
-                    except Exception as e:
-                        with open(
-                            os.path.join(
-                                log_dir, f"{config_name}_{device['hostname']}.log"
-                            ),
-                            "a",
-                        ) as log_file:
-                            log_with_timestamp(
-                                log_file, f"Error on {device['hostname']}: {e}"
-                            )
-                            progress_bar.update(len(commands))
 
-    else:
-        print("Please specify a configuration file using --cfg")
+@app.command()
+def run(config: str):
+    """
+    Specify the configuration file (without .yaml extension).
+    """
+    ensure_directories_exist()
+
+    config_file = CONFIG_DIR / f"{config}.yaml"
+    if not config_file.is_file():
+        console.print(
+            Panel(
+                f"Configuration file '{config_file}' does not exist.", style="bold red"
+            )
+        )
+        return
+
+    config_data = load_config(config_file)
+    print_config(config_data)
+
+    try:
+        if not Confirm.ask("\nDo you want to proceed with these parameters?"):
+            console.print(Panel("Run cancelled.", style="red"))
+            raise typer.Exit()
+    except KeyboardInterrupt:
+        console.print(Panel("Exiting...", style="bold red"))
+        return
+
+    print()
+
+    commands = config_data["commands"]
+    devices = config_data["devices"]
+
+    config_name = config
+
+    total_tasks = len(devices) * len(commands)
+
+    with Progress(
+        TextColumn("[progress.description]{task.description}"),
+        BarColumn(),
+        TextColumn("{task.completed}/{task.total} tasks"),
+        TimeElapsedColumn(),
+        TimeRemainingColumn(),
+    ) as progress:
+        task_id = progress.add_task("Executing commands", total=total_tasks)
+        num_devices = len(devices)
+        max_workers = min(10, num_devices)  # Set a reasonable maximum number of workers
+
+        with ThreadPoolExecutor(max_workers=max_workers) as executor:
+            futures = {
+                executor.submit(
+                    run_commands,
+                    device,
+                    commands,
+                    config_name,
+                    task_id,
+                    progress,
+                ): device
+                for device in devices
+            }
+            for future in as_completed(futures):
+                device = futures[future]
+                try:
+                    future.result()
+                except Exception as e:
+                    logger = setup_logger(config_name, device["hostname"])
+                    logger.error(f"Error on {device['hostname']}: {e}")
+                    progress.update(task_id, advance=len(commands))
 
 
 if __name__ == "__main__":
-    main()
+    app()
